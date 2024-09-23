ffee9c062e50fad36b322a42e7553ac33a81f740
duidae <duidae@gmail.com>
2024-09-23 22:17:29 +0800

    fix: replace redirect with fetch

--- packages/frontend/src/app/api/test-bypass/route.ts
+++ packages/frontend/src/app/api/test-bypass/route.ts
@@ -1,8 +1,16 @@
-import { draftMode } from 'next/headers'
-import { redirect } from 'next/navigation'
+import { draftMode, cookies } from 'next/headers'
+
+const frontendOrigin = process.env.ORIGIN
 
 export async function GET() {
   draftMode().enable()
-  console.log('Redirect to /test-bypass')
-  redirect('/test-bypass')
+  console.log('Fetch /test-bypass')
+
+  const draftCookieName = '__prerender_bypass'
+  const result = await fetch(`${frontendOrigin}/test-bypass`, {
+    headers: {
+      cookie: `${draftCookieName}=${cookies().get(draftCookieName)?.value}`,
+    },
+  })
+  return result
 }
